4aab87acfbdd2ffb16856c6849da0a511b17f297
Fabrizio Castro López <75953508+fabriziocl@users.noreply.github.com>
2024-05-12 15:24:51 -0300

    fix(metadata) Add title and description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'SWAPI Challenge',
+  title: 'SWAPI Challenge app',
   description: 'App by Fabrizio Castro Lopez',
 }
 
