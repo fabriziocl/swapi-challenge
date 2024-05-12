2e8de9def49c326c4064e56a213dfb3fe956cf3c
Fabrizio Castro López <75953508+fabriziocl@users.noreply.github.com>
2024-05-12 15:26:50 -0300

    fix(component) Refactor main element content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import Image from 'next/image'
-import LandingPage from './components/LandingPage'
+import LandingPage from './ui/LandingPage'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <LandingPage />
+    // <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="">
+        <LandingPage />
     </main>
   )
 }
