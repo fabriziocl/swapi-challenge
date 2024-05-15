a312883a07d35790bb513e12a1f037374dc6fdce
Fabrizio Castro López <75953508+fabriziocl@users.noreply.github.com>
2024-05-15 12:45:50 -0300

    add: add styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,7 @@ import LandingPage from './ui/LandingPage'
 
 export default function Home() {
   return (
-    // <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    <main className="">
+    <main className="flex min-h-screen flex-col bg-[#121212]">
         <LandingPage />
     </main>
   )
--- src/app/ui/LandingPage.tsx
+++ src/app/ui/LandingPage.tsx
@@ -6,29 +6,47 @@ import characters from '../../../public/characters.jpg'
 
 export default async function LandingPage() {
   return (
-    <section>
-      <h1>STAR WARS API challenge app</h1>
-      <h5>Welcome to the Star Wars API, where you will find information about films and characters. Let’s get started!</h5>
-      {/* convertir en array y mandar a /lib */}
-      <div className="pt-3">
-        <p>Films</p>
-        <Link href="/films">
-          <Image
-            src={films}
-            alt="films image"
-            width="500"
-          />
-        </Link>
+    <section className="lg:py-16">
+      <div className="flex flex-col items-center mb-5">
+        <h1 className="text-[#ffeb1f] mt-4 mb-4 text-4xl sm:text-5xl lg:text-8xl font-extrabold">
+          STAR WARS API challenge app
+        </h1>
+        <h5 className='text-[#ADB7BE] text-base sm:text-lg lg:text-xl mb-6 mt-4'>
+          Welcome to the Star Wars API, where you will find information about films and characters. Let’s get started!
+        </h5>
       </div>
-      <div className="pt-3">
-        <p>Characters</p>
-        <Link href="/characters">
-          <Image
-            src={characters}
-            alt="characters image"
-            width="500"
-          />
-        </Link>
+      {/* convertir en array y mandar a /lib */}
+      <div className='grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-12 mt-10'>
+        <div className='bg-[#ff212e] rounded-md lg:col-start-2 sm:w-full lg:col-span-5 place-self-center text-center justify-center p-3 my2'>
+          <p className='text-[#ADB7BE] text-base sm:text-lg lg:text-xl mt-4'>Films</p>
+          <Link
+           href="/films"
+           className="flex justify-center items-center"
+           >
+            <Image
+              className="rounded"
+              src={films}
+              alt="films image"
+              height="300"
+              objectFit="cover"
+            />
+          </Link>
+        </div>
+        <div className='bg-[#157dfc] rounded-md lg:col-start-7 sm:w-full lg:col-span-5 place-self-center text-center justify-center p-3 my-2'>
+          <p className='text-[#ADB7BE] text-base sm:text-lg lg:text-xl mt-4'>Characters</p>
+          <Link
+            href="/characters"
+            className="flex justify-center items-center"
+          >
+            <Image
+              className="rounded"
+              src={characters}
+              alt="characters image"
+              height="300"
+              objectFit="cover"
+            />
+          </Link>
+        </div>
       </div>
     </section>
   )
